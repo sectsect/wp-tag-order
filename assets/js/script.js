f3e9a0c0e628107b7f3be2375ab7ba3960e901df
SECT <info@sectwebstudio.com>
2017-11-17 02:30:02 +0900

    :sparkles: Add compatible with WordPress 4.9

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,11 +8,11 @@ jQuery(document).ready(function() {
 		Sync Tags to "#tagsdiv-post_tag" box
 	================================================== */
 	var flag = "";
-	setTimeout(function(){
+	setTimeout(function() {
 	    jQuery("[id^='tagsdiv-']").find('.tagchecklist').on('DOMSubtreeModified propertychange', function() {
-			setTimeout(jQuery.proxy(function(){
+			setTimeout(jQuery.proxy(function() {
 				var cont = jQuery(this).html();
-				if(cont == flag) {
+				if (cont == flag) {
 			        return;		//prevent multiple simultaneous triggers
 			    }
 				flag = cont;
@@ -21,7 +21,7 @@ jQuery(document).ready(function() {
 				var taxonomy = postboxid.replace('tagsdiv-', '');
 				var removedBtnString = removeElements(cont, "button");
 				var ary = new Array();
-				jQuery(removedBtnString).filter('span').each(function(){
+				jQuery(removedBtnString).filter('li, span').each(function() {
 					ary.push(jQuery(this).text().substr(1));
 				});
 				var string = ary.join();
@@ -33,12 +33,12 @@ jQuery(document).ready(function() {
 					beforeSend   : function(){
 						jQuery("#tagsdiv-" + taxonomy + " h2, #wpto_meta_box-" + taxonomy + " h2").addClass("ready");
 					}
-				}).done(function(data){
+				}).done(function(data) {
 					jQuery("#wpto_meta_box-" + taxonomy + " .inside .inner ul").html(data);
-				}).fail(function(XMLHttpRequest, textStatus, errorThrown){
+				}).fail(function(XMLHttpRequest, textStatus, errorThrown) {
 					alert("Load Error. Please Reload...");
-				}).always(function(data){
-					setTimeout(function(){
+				}).always(function(data) {
+					setTimeout(function() {
 						jQuery("#tagsdiv-" + taxonomy + " h2, #wpto_meta_box-" + taxonomy + " h2").removeClass("ready");
 					}, 300);
 				});
@@ -50,12 +50,12 @@ jQuery(document).ready(function() {
 		jQuery UI Sortable
 	================================================== */
 	jQuery(".wpto_meta_box_panel .inside .inner ul").sortable({
-		update:function(event, ui){
+		update:function(event, ui) {
 			var postboxid = jQuery(this).closest('.postbox').attr('id');
 			var taxonomy = postboxid.replace('wpto_meta_box-', '');
 
 			var ary = new Array();
-			jQuery(this).find('input[type="hidden"]').each(function(){
+			jQuery(this).find('input[type="hidden"]').each(function() {
 				var tag = jQuery(this).val();
 				ary.push(tag);
 		    });
@@ -65,15 +65,15 @@ jQuery(document).ready(function() {
 				dataType     : "json",
 				data         : {id : wto_data.post_id, nonce : wto_data.nonce, taxonomy : taxonomy, tags : string},
 				type         : "post",
-				beforeSend   : function(){
+				beforeSend   : function() {
 					jQuery("#wpto_meta_box-" + taxonomy + " h2").addClass("ready");
 				}
-			}).done(function(data){
+			}).done(function(data) {
 
-			}).fail(function(XMLHttpRequest, textStatus, errorThrown){
+			}).fail(function(XMLHttpRequest, textStatus, errorThrown) {
 				alert("Load Error. Please Reload...");
-			}).always(function(data){
-				setTimeout(function(){
+			}).always(function(data) {
+				setTimeout(function() {
 				    jQuery("#wpto_meta_box-" + taxonomy + " h2").removeClass("ready");
 				}, 300);
 			});
