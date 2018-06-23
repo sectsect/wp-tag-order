e26cc8601b05fe871f065600b0104a224ef77b90
SECT <info@sectwebstudio.com>
2018-06-23 21:32:13 +0900

    Improve tag retrieval method when tag change

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,68 +5,62 @@ jQuery(document).ready(function() {
 		return wrapped.html();
 	}
 	/*==================================================
-		Sync Tags to "#tagsdiv-post_tag" box
+	Sync Tags to "#tagsdiv-post_tag" box
 	================================================== */
 	var flag = "";
 	setTimeout(function() {
-	    jQuery("[id^='tagsdiv-']").find('.tagchecklist').on('DOMSubtreeModified propertychange', function() {
+		jQuery("[id^='tagsdiv-']").find('.tagchecklist').on('DOMSubtreeModified propertychange', function() {
 			setTimeout(jQuery.proxy(function() {
 				var cont = jQuery(this).html();
-				if (cont == flag) {
-			        return;		//prevent multiple simultaneous triggers
-			    }
+				if (cont === flag) {
+					return;  //prevent multiple simultaneous triggers
+				}
 				flag = cont;
 
 				var postboxid = jQuery(this).closest('.postbox').attr('id');
-				var taxonomy = postboxid.replace('tagsdiv-', '');
-				var removedBtnString = removeElements(cont, "button");
-				var ary = new Array();
-				jQuery(removedBtnString).filter('li, span').each(function() {
-					ary.push(jQuery(this).text().substr(1));
-				});
-				var string = ary.join();
+				var t = postboxid.replace('tagsdiv-', '');
+				var s = jQuery(this).siblings().find('textarea.the-tags').val();
 				jQuery.ajax({
 					url          : wto_data.plugin_dir_url + "includes/sync-tags.php",
 					dataType     : "json",
-					data         : {id : wto_data.post_id, nonce : wto_data.nonce, taxonomy : taxonomy, tags : string},
+					data         : {id : wto_data.post_id, nonce : wto_data.nonce, taxonomy : t, tags : s},
 					type         : "post",
 					beforeSend   : function(){
-						jQuery("#tagsdiv-" + taxonomy + " h2, #wpto_meta_box-" + taxonomy + " h2").addClass("ready");
+						jQuery("#tagsdiv-" + t + " h2, #wpto_meta_box-" + t + " h2").addClass("ready");
 					}
 				}).done(function(data) {
-					jQuery("#wpto_meta_box-" + taxonomy + " .inside .inner ul").html(data);
+					jQuery("#wpto_meta_box-" + t + " .inside .inner ul").html(data);
 				}).fail(function(XMLHttpRequest, textStatus, errorThrown) {
 					alert("Load Error. Please Reload...");
 				}).always(function(data) {
 					setTimeout(function() {
-						jQuery("#tagsdiv-" + taxonomy + " h2, #wpto_meta_box-" + taxonomy + " h2").removeClass("ready");
+						jQuery("#tagsdiv-" + t + " h2, #wpto_meta_box-" + t + " h2").removeClass("ready");
 					}, 300);
 				});
 			}, this), 20);
-	    });
+		});
 	}, 400);
-
 	/*==================================================
-		jQuery UI Sortable
+	jQuery UI Sortable
 	================================================== */
 	jQuery(".wpto_meta_box_panel .inside .inner ul").sortable({
 		update:function(event, ui) {
 			var postboxid = jQuery(this).closest('.postbox').attr('id');
-			var taxonomy = postboxid.replace('wpto_meta_box-', '');
+			var t = postboxid.replace('wpto_meta_box-', '');
 
 			var ary = new Array();
 			jQuery(this).find('input[type="hidden"]').each(function() {
 				var tag = jQuery(this).val();
 				ary.push(tag);
-		    });
+			});
 			var string = ary.join();
 			jQuery.ajax({
 				url          : wto_data.plugin_dir_url + "includes/ajax-update.php",
 				dataType     : "json",
-				data         : {id : wto_data.post_id, nonce : wto_data.nonce, taxonomy : taxonomy, tags : string},
+				data         : {id : wto_data.post_id, nonce : wto_data.nonce, taxonomy : t, tags : string},
 				type         : "post",
 				beforeSend   : function() {
-					jQuery("#wpto_meta_box-" + taxonomy + " h2").addClass("ready");
+					jQuery("#wpto_meta_box-" + t + " h2").addClass("ready");
 				}
 			}).done(function(data) {
 
@@ -74,9 +68,9 @@ jQuery(document).ready(function() {
 				alert("Load Error. Please Reload...");
 			}).always(function(data) {
 				setTimeout(function() {
-				    jQuery("#wpto_meta_box-" + taxonomy + " h2").removeClass("ready");
+					jQuery("#wpto_meta_box-" + t + " h2").removeClass("ready");
 				}, 300);
 			});
-	    }
+		}
 	});
 });
