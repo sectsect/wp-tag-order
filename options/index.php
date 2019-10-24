1c703777ff4ee690f55a4a9470c3ffb5e97d6926
SECT <info@sectwebstudio.com>
2019-10-25 00:41:34 +0900

    Code formatting and fix a typo

--- options/index.php
+++ options/index.php
@@ -4,21 +4,21 @@
 	<section>
 		<form method="post" action="">
 			<table class="form-table">
-	            <tbody>
+				<tbody>
 					<tr>
-	                    <th scope="row" style="width: 300px;">
-	                        <label for="apply" style="font-size: 14px; margin: 0;">Apply to all existing post</label>
+						<th scope="row" style="width: 300px;">
+							<label for="apply" style="font-size: 14px; margin: 0;">Apply to all existing posts</label>
 							<p style="font-size: 10px;color: #999;margin: 5px 0 0;">
 								Please be sure to backup your database before running.<br>
 								This simple process may takes a few minutes and ensures that your website can be rolled back quickly and safely if any issues arise.
 							</p>
-	                    </th>
-	                    <td style="vertical-align: top;">
-	                    	<input name="apply" id="apply" class="button button-primary" type="submit" value="Apply">
-	                    </td>
-	                </tr>
-	            </tbody>
-	        </table>
+						</th>
+						<td style="vertical-align: top;">
+							<input name="apply" id="apply" class="button button-primary" type="submit" value="Apply">
+						</td>
+					</tr>
+				</tbody>
+			</table>
 		</form>
 	</section>
 </div>
