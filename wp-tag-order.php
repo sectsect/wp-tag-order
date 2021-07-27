8eb0217fdcfc631b7b14e028c2c6469472e9ab46
SECT <info@sectwebstudio.com>
2021-07-27 16:33:45 +0900

    🎉 Bump version to v1.3.2

--- wp-tag-order.php
+++ wp-tag-order.php
@@ -7,7 +7,7 @@
  * Author URI:      https://www.ilovesect.com/
  * Text Domain:     wp-tag-order
  * Domain Path:     /languages
- * Version:         1.3.1
+ * Version:         1.3.2
  *
  * @package         WP_Tag_Order
  */
