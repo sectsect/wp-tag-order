abeb968a0f945bffd4eca25a9b92bfc67e473b84
SECT <info@sectwebstudio.com>
2021-09-19 22:27:51 +0900

    Migrate 'eslint-config-airbnb-typescript' to v14

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
     project: './tsconfig.json',
   },
   extends: [
+    'airbnb',
     'airbnb-typescript',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
