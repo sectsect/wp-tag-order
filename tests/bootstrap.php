435d22ab7bd957f6a8d9afbc3a4cbe552e78bd29
SECT <sect@sect.me>
2022-06-14 19:12:42 +0900

    Fix build error on CI

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,8 +5,6 @@
  * @package My_Plugin
  */
 
-require dirname( dirname( __FILE__ ) ) . '/vendor/yoast/phpunit-polyfills/phpunitpolyfills-autoload.php';
-
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 
 if ( ! $_tests_dir ) {
