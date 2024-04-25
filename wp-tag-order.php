352ef5d9d328cdd68b9478772e9abf95f5754c1d
sect <sect@sect.me>
2024-04-25 19:37:30 +0900

    chore: bump version to v3.4.0

--- wp-tag-order.php
+++ wp-tag-order.php
@@ -7,7 +7,7 @@
  * Author URI:      https://github.com/sectsect
  * Text Domain:     wp-tag-order
  * Domain Path:     /languages
- * Version:         3.3.0
+ * Version:         3.4.0
  *
  * @package         WP_Tag_Order
  */
