31ab9870f33f86de35c1fe8ad23c1ff427956af5
sect <sect@sect.me>
2024-04-26 18:17:56 +0900

    test: code formatting

--- tests/FunctionTests.php
+++ tests/FunctionTests.php
@@ -100,15 +100,15 @@ class FunctionTests extends WP_UnitTestCase {
 				'public'     => true,
 				'taxonomies' => array(
 					'post_tag',
-					'news_tag'
-				)
+					'news_tag',
+				),
 			)
 		);
 		register_taxonomy(
 			'news_tag',
 			'news',
 			array(
-				'hierarchical' => false
+				'hierarchical' => false,
 			)
 		);
 
