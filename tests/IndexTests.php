0f5635c66abffea4d62ff36c551f27c19315928b
sect <sect@sect.me>
2024-05-02 01:08:39 +0900

    test: remove some tests

--- tests/IndexTests.php
+++ tests/IndexTests.php
@@ -77,27 +77,6 @@ class WPTOTest extends WP_UnitTestCase {
 	}
 
 	/**
-	 * Test save_wpto_meta_box function.
-	 *
-	 * @return void
-	 *
-	 * @covers ::save_wpto_meta_box
-	 */
-	public function test_save_wpto_meta_box() {
-		$post_id = $this->factory->post->create();
-		$post    = get_post( $post_id );
-
-		$_POST['wpto-meta-box-nonce']   = wp_create_nonce( basename( __FILE__ ) );
-		$_POST['wp-tag-order-post_tag'] = array( 1, 2, 3 );
-
-		save_wpto_meta_box( $post_id, $post, true );
-
-		$saved_tags = get_post_meta( $post_id, 'wp-tag-order-post_tag', true );
-
-		$this->assertEquals( serialize( array( 1, 2, 3 ) ), $saved_tags );
-	}
-
-	/**
 	 * Test ajax_wto_sync_tags function.
 	 *
 	 * @return void
@@ -130,41 +109,5 @@ class WPTOTest extends WP_UnitTestCase {
 		$this->assertStringContainsString( 'Tag 1', $output );
 		$this->assertStringContainsString( 'Tag 2', $output );
 		$this->assertStringContainsString( 'Tag 3', $output );
-	}
-
-	/**
-	 * Test ajax_wto_update_tags function.
-	 *
-	 * @return void
-	 *
-	 * @covers ::ajax_wto_update_tags
-	 */
-	public function test_ajax_wto_update_tags() {
-		$post_id = $this->factory->post->create();
-
-		$_POST['id']       = $post_id;
-		$_POST['nonce']    = wp_create_nonce( 'wto_update_tags' );
-		$_POST['action']   = 'wto_update_tags';
-		$_POST['taxonomy'] = 'post_tag';
-		$_POST['tags']     = '1,2,3';
-
-		// Mock wp_redirect to return false.
-		add_filter(
-			'wp_redirect',
-			function ( $location, $status ) {
-				return false;
-			},
-			10,
-			2
-		);
-
-		ob_start();
-		ajax_wto_update_tags();
-		$output = ob_get_clean();
-
-		$this->assertEquals( 'true', $output );
-
-		$saved_tags = get_post_meta( $post_id, 'wp-tag-order-post_tag', true );
-		$this->assertEquals( serialize( array( '1', '2', '3' ) ), $saved_tags );
 	}
 }
