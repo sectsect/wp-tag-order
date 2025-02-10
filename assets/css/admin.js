2ad9cbe28d946ccd98b3eb6f7a4f3a23424461eb
sect <sect@sect.me>
2025-02-10 18:24:42 +0900

    chore(deps): update dependencies

--- assets/css/admin.js
+++ assets/css/admin.js
@@ -29,13 +29,13 @@ return module.exports;
 // webpack/runtime/rspack_version
 (() => {
 __webpack_require__.rv = function () {
-	return "1.1.8";
+	return "1.2.2";
 };
 
 })();
 // webpack/runtime/rspack_unique_id
 (() => {
-__webpack_require__.ruid = "bundler=rspack@1.1.8";
+__webpack_require__.ruid = "bundler=rspack@1.2.2";
 
 })();
 /************************************************************************/
